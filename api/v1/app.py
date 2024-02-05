6eb6a78f585e84cf621ac09c3da2a39cdf1c63b7
Abdeljalil37 <widadpop@gmail.com>
2024-02-06 00:29:17 +0100

    add error message for task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """ setup the app  for the api """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+        404:
+    description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
